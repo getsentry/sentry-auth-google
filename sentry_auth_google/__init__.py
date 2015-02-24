9a2e862532ede1b8f95db0e52a2697e898b2962d
David Cramer <dcramer@gmail.com>
2015-02-24 14:02:14 -0800

    Correct import

--- sentry_auth_google/__init__.py
+++ sentry_auth_google/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from sentry.auth import register
 
-from .providers.google_oauth2 import GoogleOAuth2Provider
+from .provider import GoogleOAuth2Provider
 
 register('google', GoogleOAuth2Provider)
