c9db30cc01f2a801ee45c140d543b6dc13efb17a
David Cramer <dcramer@gmail.com>
2016-02-16 12:57:27 -0800

    0.2.0.dev0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ tests_require = [
 
 setup(
     name='sentry-auth-google',
-    version='0.1.0',
+    version='0.2.0.dev0',
     author='Sentry',
     author_email='support@getsentry.com',
     url='https://www.getsentry.com',
